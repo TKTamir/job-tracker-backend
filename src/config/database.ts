e37f814d4e257020cfe482fad0423574950267d2
tamir <tamirkhy@gmail.com>
2025-02-10 17:48:00 +0200

    Use ssl in database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     port: Number(process.env.DB_PORT),
     logging: false,
   }
