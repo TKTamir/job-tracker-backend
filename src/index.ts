3746b121cc53c35fc2c9d310b3deae96233d9c5d
tamir <tamirkhy@gmail.com>
2025-02-10 17:14:45 +0200

    Implement routes and initDB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {initDB} from "./models";
+import userRoutes from "./routes/userRoutes";
+import jobRoutes from "./routes/jobRoutes";
 
 dotenv.config();
 
@@ -8,3 +11,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/users", userRoutes);
+app.use("/api/jobs", jobRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
