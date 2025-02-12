95fa9fadb63116472d634de446da66a494174d6c
tamir <tamirkhy@gmail.com>
2025-02-12 17:44:13 +0200

    Allow localhost:5173 origin

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
 
 app.use("/api/users", userRoutes);
 app.use("/api/jobs", jobRoutes);
