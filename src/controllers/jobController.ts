c1446b144b4e0d5ad1c412b29563ec90b3bc0444
tamir <tamirkhy@gmail.com>
2025-04-24 17:00:49 +0300

    Fix job and user imports

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import Job from "../models/Job";
+import {Job} from "../models";
 
 export const createJob = async (req: Request, res: Response): Promise<void> => {
   try {
