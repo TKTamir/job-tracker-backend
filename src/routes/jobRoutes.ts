cb5f43b670c622659bc472f183d01ad5534a1718
tamir <tamirkhy@gmail.com>
2025-05-05 16:07:27 +0300

    Implement authenticatetoken in jobroutes

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -5,12 +5,13 @@ import {
   getJobById,
   updateJob,
   deleteJob,
+  authenticateToken
 } from "../controllers/jobController";
 
 const router = express.Router();
 
-router.post("/", createJob);
-router.get("/", getJobs);
+router.post("/", authenticateToken, createJob);
+router.get("/", authenticateToken, getJobs)
 router.get("/:id", getJobById);
 router.put("/:id", updateJob);
 router.delete("/:id", deleteJob);
