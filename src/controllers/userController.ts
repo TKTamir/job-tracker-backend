6d17b4acc8cf0e502efb9629f288905f1be274d2
tamir <tamirkhy@gmail.com>
2025-05-06 15:07:19 +0300

    Return error message with 500 response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -12,8 +12,8 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     const user = await User.create({name, email, password: hashedPassword});
 
     res.status(201).json({message: "User registered successfully", user});
-  } catch (error) {
-    res.status(500).json({message: "Error registering user"});
+  } catch (error: unknown) {
+    res.status(500).json({message: "Error registering user: ", error: (error as Error).message});
   }
 };
 
@@ -32,7 +32,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     });
 
     res.json({message: "Login successful", token});
-  } catch (error) {
-    res.status(500).json({message: "Error logging in"});
+  } catch (error: unknown) {
+    res.status(500).json({message: "Error logging in: ", error: (error as Error).message});
   }
 };
