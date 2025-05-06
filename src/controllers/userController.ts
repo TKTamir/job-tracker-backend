e36995141469d89f472f7562bb0bac51703d4e41
tamir <tamirkhy@gmail.com>
2025-05-06 15:08:28 +0300

    Return user token instead of user object from register route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,8 +10,12 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({name, email, password: hashedPassword});
+    
+    const token = jwt.sign({id: user.id, email: user.email, name: user.name}, process.env.JWT_SECRET as string, {
+      expiresIn: "1h",
+    });
 
-    res.status(201).json({message: "User registered successfully", user});
+    res.status(201).json({message: "User registered successfully", token});
   } catch (error: unknown) {
     res.status(500).json({message: "Error registering user: ", error: (error as Error).message});
   }
