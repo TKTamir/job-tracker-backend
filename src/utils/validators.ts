5b5ab82a30827fd4366610e1614cec31122c0c30
tamir <tamirkhy@gmail.com>
2025-05-11 17:30:54 +0300

    Create isValidEmail validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -2,3 +2,8 @@ export const isStrongPassword = (password: string): boolean => {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])[A-Za-z\d@$!%*?#&]{8,}$/;
   return passwordRegex.test(password);
 };
+
+export const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
